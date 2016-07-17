af49514ae1af92fc767b24f537cab9b6da08d44c
Anton Ukhanev <xedin.unknown@gmail.com>
2016-07-17 16:04:40 +0200

    Added `CanAssertThatTrait`.

---
+++ src/CanAssertThatTrait.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Dhii\Test;
+
+/**
+ * Base trait that contains functionalty common to all traits which require the class to be a PHPUnit constraint.
+ *
+ * @since [*next-version*]
+ */
+trait CanAssertThatTrait
+{
+    /**
+     * @codeCoverageIgnore
+     * @since [*next-version*]
+     */
+    abstract public function assertThat($value, \PHPUnit_Framework_Constraint $constraint, $message = '');
+}
