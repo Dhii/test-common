2ecf34cd9f03041555455a64b9a8a6fa918e765f
Anton Ukhanev <xedin.unknown@gmail.com>
2016-07-18 12:01:23 +0200

    Added `AssertionSuccess` constraint.
    Tests included.

---
+++ src/Constraint/AssertionSuccess.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Dhii\Test\Constraint;
+
+/**
+ * Fails if passed callback contains a failed assertion.
+ *
+ * @since [*next-version*]
+ */
+class AssertionSuccess extends \PHPUnit_Framework_Constraint
+{
+    /**
+     * Runs the callback, and sees if assertions in it don't fail.
+     *
+     * @since [*next-version*]
+     *
+     * @param callable $other The function that must contani a failed assertion.
+     *
+     * @return bool False if function contains a failed assertion; true otherwise.
+     */
+    public function matches($other)
+    {
+        if (!is_callable($other)) {
+            throw new \PHPUnit_Framework_Exception(sprintf('Callable specified for %1$s is not a valid callable', get_class($this)));
+        }
+
+        try {
+            call_user_func_array($other, []);
+        } catch (\PHPUnit_Framework_ExpectationFailedException $ex) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @since [*next-version*]
+     *
+     * @return string String representation of this constraint.
+     */
+    public function toString()
+    {
+        return 'does not contain a failed assertion';
+    }
+}
---
+++ test/unit/Constraint/AssertionSuccessTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Dhii\Test\Test\Constraint;
+
+use Dhii\Test;
+
+/**
+ * Testing AssertionSuccess PHPUnit constraint.
+ *
+ * This constraint matches if the function it is testing does not contain a failed assertion.
+ *
+ * @since [*next-version*]
+ */
+class AssertionSuccessTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Creates a new constraint instance.
+     *
+     * @since [*next-version*]
+     *
+     * @return \Dhii\Test\Constraint\AssertionFailure The constraint instance.
+     */
+    public function createConstraint()
+    {
+        $constraint = new Test\Constraint\AssertionSuccess();
+
+        return $constraint;
+    }
+
+    /**
+     * Tests whether a valid constraint can be instantiated.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $constraint = $this->createConstraint();
+        $this->assertInstanceOf(\PHPUnit_Framework_Constraint::class, $constraint,
+            'Constraint is not a valid constraint');
+    }
+
+    /**
+     * Tests whether the constraint can successfully match a successful assertion.
+     *
+     * @since [*next-version*]
+     */
+    public function testMatchSuccess()
+    {
+        $constraint = $this->createConstraint();
+        $function = function () {
+            $this->assertTrue(true, 'This must succeed');
+        };
+        $result = $constraint->matches($function);
+        $this->assertTrue($result, 'Constraint must match successful assertion');
+    }
+
+    /**
+     * Tests whether the constraint can fail matching a failed assertion.
+     *
+     * @since [*next-version*]
+     */
+    public function testMatchFailure()
+    {
+        $constraint = $this->createConstraint();
+        $function = function () {
+            $this->assertTrue(false, 'This must fail');
+        };
+        $result = $constraint->matches($function);
+        $this->assertFalse($result, 'Constraint must not match a failed assertion');
+    }
+
+    /**
+     * Tests whether the constraint throws the correct exception when given an incorrect subject.
+     *
+     * @since [*next-version*]
+     */
+    public function testNonCallableSubject()
+    {
+        $this->expectException(\PHPUnit_Framework_Exception::class);
+        $this->expectExceptionMessage('not a valid callable');
+
+        $constraint = $this->createConstraint();
+        $function = 'not a callable';
+        $constraint->matches($function);
+    }
+
+    /**
+     * Tests that the string representation of the constraint is correct.
+     *
+     * @since [*next-version*]
+     */
+    public function testToString()
+    {
+        $constraint = $this->createConstraint();
+        $this->assertInternalType('string', $constraint->toString(), 'The constraint string must be a valid string');
+    }
+}
